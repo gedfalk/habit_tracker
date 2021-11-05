b6a895480e5c651ce62bfd7cd04cd83741d4eedf
gedfalk <blite.co.stn@gmail.com>
2021-11-06 00:11:15 +0300

    gedfalk archive almost done

--- archive/views.py
+++ archive/views.py
@@ -3,8 +3,15 @@ from django.shortcuts import render, get_object_or_404
 from .models import Session
 
 def session_list(request):
+    context = {}
     sessions = Session.objects.all()
-    return render(request, 'archive/list.html', {'sessions': sessions})
+    context['sessions'] = sessions
+
+    context['colors'] = {'blue': ['779BED', 'A1BCFF'],
+                         'green': ['xxxxxxx', 'xxxxxxx'],
+                         'violet': ['xxxxxxx', 'xxxxxxx']}
+
+    return render(request, 'archive/list.html', context)
 
 
 
--- tracker/models.py
+++ tracker/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.db.models.fields import NullBooleanField
 
 class Activity(models.Model):
     id_activity = models.AutoField(primary_key=True)
     name_activity = models.CharField(max_length=30)
     shortname_activity = models.CharField(max_length=3, default='OOO')
     unit = models.SmallIntegerField()
+    color = models.CharField(max_length=20, default='white')
     # id_table
 
     def __str__(self):
