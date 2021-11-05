fc64f300600fd264fed2fa79467a3fe0b174c7c3
gedfalk <blite.co.stn@gmail.com>
2021-11-05 18:25:37 +0300

    ooops

--- project/settings.py
+++ project/settings.py
@@ -19,6 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-kezug+g)ul*b!13b_$gq6tb(gf@f4ppch@2ds)q414jrz**gji'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
