3bdf1f09badf782837b10e1e8a61e2a2fd8cb63a
gedfalk <blite.co.stn@gmail.com>
2021-11-10 23:42:10 +0300

    minor date change

--- project/settings.py
+++ project/settings.py
@@ -107,13 +107,14 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+#LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'     # разобраться с датами
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_L10N = True
+USE_L10N = False            # с этим нужно будет покопаться
 
 USE_TZ = True
 
