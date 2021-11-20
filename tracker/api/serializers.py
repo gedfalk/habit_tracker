3fee4938925258d4290f37d687da4d9d61b5de62
gedfalk <blite.co.stn@gmail.com>
2021-11-20 22:54:20 +0300

    adding SessionSerializer

--- tracker/api/serializers.py
+++ tracker/api/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
 from archive.models import Session
+import time, datetime
 
 class SessionSerializer(serializers.ModelSerializer):
+    in_seconds = serializers.SerializerMethodField(method_name='calculate_seconds')
+    
     class Meta:
         model = Session
-        #fields = ['datte', 'id_activity', 'title']
-        fields = '__all__'
+        fields = ['datte', 'id_activity', 'title', 'in_seconds']
+    
+    # перевод даты в секунды
+    def calculate_seconds(self, instance):
+        return int(time.mktime(instance.datte.timetuple()))
